4dc9f1506223a89b81121de5fbef21f39ad5e563
Daniel Tkachenko <tkachdan@fel.cvut.cz>
2015-04-18 10:57:19 +0200

    Test

---
+++ src/main/java/test/App.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("She said \"Hello!\" to me. \n \"I was glad to see her\"");
+        System.out.println("Branch myBranch");
+        int a = 5;
+    }
+}
