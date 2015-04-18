36183c4faf3270cd70339e5ab9ece43d8aa68ba1
Daniel Tkachenko <tkachdan@fel.cvut.cz>
2015-04-18 10:58:05 +0200

    newBranch

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -6,7 +6,7 @@ package test;
 public class App {
     public static void main(String[] args) {
         System.out.println("She said \"Hello!\" to me. \n \"I was glad to see her\"");
-        System.out.println("Branch myBranch");
+        System.out.println("Branch newBranch");
         int a = 5;
     }
 }
